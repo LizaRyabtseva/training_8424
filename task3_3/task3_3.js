56061be86af2ac4fc233604ca2ada63165686c88
lizaryabtseva <lizaryabtsevav2@gmail.com>
2020-09-02 18:24:06 +0300

    Задания из 3 главы

--- task3_3/task3_3.js
+++ task3_3/task3_3.js
@@ -1,3 +1,13 @@
+function countBs(str) {
+    let res=0;
+    for (let v of str) {
+        if (v="B")  res++;
+    }
+    return res;
+}
+
+console.log(countBs("sbbbs"));
+
 function countChar(str, ch) {
     let res=0;
     for (let v of str) {
