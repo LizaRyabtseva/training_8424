54924da470e321b490597bc6ce4f01244560bb6a
lizaryabtseva <lizaryabtsevav2@gmail.com>
2020-10-21 17:53:56 +0300

    Исправленное задание 14_1. 21.10.2020

--- task14_1/task14_1.js
+++ task14_1/task14_1.js
@@ -8,13 +8,6 @@ const mountains = [
     {name: "Mont Blanc", height: 4808, place: "Italy/France"}
 ];
 
-function createStyle(elems) {
-    for (let i=0; i<elems.length; i++) {
-        elems[i].style.border = 2 + 'px solid';
-        elems[i].style.textAlign = 'right';
-    }
-}
-
 function createTable(mountains) {
     let table = document.createElement('table');
     table.style.border = 2 + 'px solid';
@@ -31,22 +24,26 @@ function createTable(mountains) {
     let ths = table.getElementsByTagName('th');
     let trs = table.getElementsByTagName('tr');
 
-    for (let i = 0; i < keys.length; i++) {
-        ths[i].style.padding = 15 + 'px';
-    }
-
-    for (let i = 1; i < trs.length; i++) {
-        trs[i].appendChild(document.createElement('td')).appendChild(document.createTextNode(mountains[i-1].name));
-        trs[i].appendChild(document.createElement('td')).appendChild(document.createTextNode(mountains[i-1].height));
-        trs[i].appendChild(document.createElement('td')).appendChild(document.createTextNode(mountains[i-1].place));
+    for (let i=1; i<trs.length; i++) {
+        for (let j=0; j<keys.length; j++) {
+            trs[i].appendChild(document.createElement('td')).appendChild(document.createTextNode(mountains[i-1][keys[j]]));
+        }
     }
 
     let tds = table.getElementsByTagName('td');
-    for (let i = 0; i < 3 *mountains.length; i++) {
-        tds[i].style.padding = 15 + 'px';
+
+    for (let td of tds) {
+        if (td.innerHTML==+td.innerHTML) {
+            td.style.textAlign = 'right';
+        }
+        td.style.padding = 15 + 'px';
+        td.style.border = 2 + 'px solid';
     }
-    createStyle(tds);
-    createStyle(ths);
+
+    for (let th of ths) {
+        th.style.padding = 15 + 'px';
+        th.style.border = 2 + 'px solid';
+    }
 
     return table;
 }
