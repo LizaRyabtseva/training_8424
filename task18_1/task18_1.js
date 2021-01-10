5c5c8cb7e7ae02effd673af85f5c95e4c59b89fd
lizaryabtseva <lizaryabtsevav2@gmail.com>
2021-01-10 17:32:26 +0300

    Задание из главы 18 10.01.2021

--- task18_1/task18_1.js
+++ task18_1/task18_1.js
@@ -1,12 +1,8 @@
 const fetch=require("node-fetch");
 const types=["text/plain", "text/html", "application/json", "application/rainbows+unicorns"];
 
-(async ()=> {
-    let res;
-    for (let type of types) {
-        res= await fetch("https://eloquentjavascript.net/author", {headers: {Accept: type}});
-        `${type}`==="application/rainbows+unicorns" ?
-            console.log(`Тип: ${type}, Содержимое: ${await res.text()}, Статус: ${await res.status}`) :
-            console.log(`Тип: ${type}, Содержимое: ${await res.text()}`);
-        }
-})();
+for (let type of types) {
+    fetch("https://eloquentjavascript.net/author", {headers: {Accept: type}})
+        .then(res => res.text())
+        .then(content => console.log(`Тип: ${type}, Содержимое: ${content}`));
+}
